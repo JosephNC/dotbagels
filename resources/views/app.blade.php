baa57ec43c2c39c3eccc198df18c7af6d5a3cab5
JosephNC <chukwudinwakpaka@hotmail.com>
2021-04-13 17:47:03 -0700

    Updated app.blade.php and added favicon.ico for browsers.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,16 +6,18 @@
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset( 'favicon.ico' ) }}" />
+
     <link rel="preconnect" href="https://fonts.gstatic.com">
 
     <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@200;300;400;600;700;800;900&display=swap" rel="stylesheet">
 
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
-    {{-- Inertia --}}
+    {{-- Inertia.js polyfills --}}
     <script src="https://polyfill.io/v3/polyfill.min.js?features=smoothscroll,NodeList.prototype.forEach,Promise,Object.values,Object.assign" defer></script>
 
-    {{-- Ping CRM --}}
+    {{-- startsWith polyfill --}}
     <script src="https://polyfill.io/v3/polyfill.min.js?features=String.prototype.startsWith" defer></script>
 
     <script src="{{ mix('/js/app.js') }}" defer></script>
